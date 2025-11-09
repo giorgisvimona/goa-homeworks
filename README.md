0a0ea72b5cd9e80e5ed66e3507954f2c2873cdb2
giorgisvimona <giorgisvimonishviliknq@britishuni.edu.ge>
2025-11-09 20:00:33 +0400

    second commit

--- README.md
+++
@@ -1,2 +0,0 @@
-"# goa-homeworks" 
-"# goa-homeworks" 
--- day11/lesson.py
+++ day11/lesson.py
@@ -1,4 +1,5 @@
 random_list=["giorgi","apple","saba","banana"]
+
 print(random_list)
 print(random_list[0],random_list[1],random_list[3])
 print(random_list[-4])
