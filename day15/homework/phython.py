222bd95b1c0422ae3d4421df9a65219c71139705
giorgisvimona <giorgisvimonishviliknq@britishuni.edu.ge>
2025-11-25 21:04:56 +0400

    second commit

---
+++ day15/homework/phython.py
@@ -0,0 +1,52 @@
+
+
+i=1
+
+while i<20:
+    i+=1
+    print(i)
+
+
+    i=-20
+
+    while i<10:
+        i+=1
+        print(i)
+
+
+i= 1
+while i < 20:
+    if i % 2 == 0:
+        print(i)
+    i += 1
+
+   
+i=0
+
+while i<20:
+    i+=2
+    print(i)
+
+i=1
+o=0
+
+while i<=20:
+    o+=i 
+    i+=1
+    print("ჯამი",o)
+
+
+
+i=1
+o=0
+while i<=20:
+    o*=i
+    i*=0
+    print(o)
+
+    
+
+ 
+
+
+
