0b9f239c7735aade55266a3fb2a0620cf74ca130
giorgisvimona <giorgisvimonishviliknq@britishuni.edu.ge>
2026-01-10 12:19:13 +0400

    second commit

---
+++ day28/cw.py
@@ -0,0 +1,37 @@
+
+
+lst = ["Gamarjoba",  6, 8.9, True]
+lst.append("buaa")
+print(lst)
+
+list1=["gamarjoba"]
+print(len(list1))
+
+list2=[1,2,3,4,5]
+print(len(list2))
+
+list3=[ "I love python"]
+print(list3.split())
+
+list4 = ['h', 'j', 'k']
+print(list4.join())
+
+text = "hello world"   
+result = text.upper()  
+print(result)
+
+text = "PYTHON IS FUN"
+result = text.lower()
+print(result)
+
+text = "python"
+result = text.capitalize()
+print(result)
+
+
+
+list5 =  [1, 2, 3]
+print(list5.remove(3))
+
+var1 = "Gamarjoba", var2 = 5, var3 = 6.7, var4 = True
+print(type.var1)
